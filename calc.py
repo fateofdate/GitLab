557f7e3fdbc3718d992a53997950849f38479421
51asm <yankail520@gmail.com>
2025-10-03 17:10:31 +0800

    class Cargo, class CManager

---
+++ calc.py
@@ -0,0 +1,20 @@
+import math
+
+
+class Cargo:
+    pass
+
+
+
+
+class CManager:
+    def __init__(self) -> None:
+        pass
+
+    def _get_input(self) -> str:
+
+        return ""
+
+# CManager 
+
+
